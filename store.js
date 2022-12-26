6b0305ea3249537c1210822d872878c545b1ba50
rafalos99 <rafsanzaniludhi@gmail.com>
2022-12-26 18:57:44 +0000

    added event listeners remove buttons. Cart total

--- store.js
+++ store.js
@@ -1,8 +1,20 @@
+// select all buttons with btn-danger class into a variable
 var removeCartItemButtons = document.getElementsByClassName('btn-danger')
 console.log(removeCartItemButtons)
+// need to loop through these objects using a for loop (.length = number of remove buttons there is here)
 for (var i = 0; i < removeCartItemButtons.length; i++) {
     var button = removeCartItemButtons[i]
-    button.addEventListener('click', function() {
-        console.log('clicked')
+    button.addEventListener('click', function(event) {
+        console.log('clicked') //now when we click the remove buttons it logs that on the console
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove() // two parent elements to select entire parent element holding the cart. remove to delete items off cart.
     })
+}
+
+//want to find the price and multiply by quantity of items for total cost 
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i]
 }
